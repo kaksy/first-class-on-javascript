8f8080e8cdc673d4e41163c9821c34ad18a02fce
unknown <nsikakatan@gmail.com>
2022-06-07 18:15:44 -0700

    my first class on javascript

---
+++ index.js
@@ -0,0 +1,42 @@
+    //  alert('hello world!!!');
+ [1,2]. forEach(alert);
+// console.log('hllo guys!!!');
+// document.write('hello world');
+// document.write('ask google!!!');
+
+// var name;
+// name= 'cvhioma';
+// document.write(name);
+let user = 'gold';
+user ='cody'; 
+user  ='strota';
+
+console.log(user);
+
+
+// const gender = 'male';
+ 
+
+let student = `female`;
+console .log(typeof student);
+
+let value = null;
+console .log(typeof value);
+
+let user1 = {
+    name : 'atan',
+    gender: 'male',
+    age : 34,
+    tall : false
+};
+ console.log(user1)
+
+
+//  let name = prompt('what is your name ')
+//  alert (name)
+
+let age = confirm('do you now her name')
+alert (age)
+
+let name = ('define')
+console.log(typeof name)
